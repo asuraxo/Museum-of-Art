1551197941563de700609660b2b66cff92dd320f
asuraxo <asuraxo@gmail.com>
2023-04-06 22:28:54 -0400

    devDependency

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
+*  WEB422 – Assignment 6
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Wai Chung, So Student ID: 147446215 Date: Mar 24, 2023
+*  Name: Wai Chung, So Student ID: 147446215 Date: Apr 7, 2023
 *
 *
 ********************************************************************************/ 
